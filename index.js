bf489bd55cd5de5c4a47a1513ba55cb47bcb2a53
shiveshnavin <shiveshnavin@gmail.com>
2018-01-24 00:38:25 +0530

    figure out binding issue

--- index.js
+++ index.js
@@ -1,56 +1,40 @@
+ 
 var express = require('express')
 var Handlebars = require('handlebars');
 var fs=require('fs')
 var app = express()
 var mysql=require('mysql')
+var mos=require('mustache')
+var mustache=require('mustache')
 
 var sql=mysql.createConnection({
-
   host:"127.0.0.1",
   user:"root",
   password:"pass",
   database:"exploman"
-
 })
-
 var path=__dirname+"/views"
-
 var exec=function(sql_str,callback){
-
     console.log("Executing : "+sql_str)
-
-    sql.query(sql_str,function(err,result){
-      
+    sql.query(sql_str,function(err,result){     
       if(err) throw err
       callback(result)
-
     })
-
-
 }
-
-
 sql.connect(function(err){
 
   if(err) throw err
   console.log("Mysql Connection Success !")
    
 })
-
-
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
-
 /**********************/
-
 let compilehtml=function(source,data){
-
   if(source==null)
       source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
       "{{kids.length}} kids:</p>" +
       "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
-
-
   if(data==null)
       data = { "name": "Alan", "hometown": "Somewhere, TX",
       "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
@@ -68,7 +52,7 @@ let compilehtml=function(source,data){
 
 app.get('/', function(request, response) {
   console.log('HTTP Call Triggred !')
-  response.send('Hello World!'+request.getElementsByTagName(''))
+  response.send('Hello World!')
 })
 
 
@@ -83,7 +67,7 @@ app.get('/sql',function(req,resp){
         console.log("SQL Result SUCCESS ")
         resp.json((result))
       })
-  /**/
+  
     }
 })
 
@@ -120,10 +104,15 @@ app.get('/get_conf',function(req,resp){
   
 })
 
+var hbs = require('express-handlebars').create();
+ 
 app.get('/test',function(req,res){
 
-  var html_test=fs.readFileSync(path+'/empty/test.html')
-  res.sendFile(compilehtml(html_test))
+  var html_test=fs.readFileSync(path+'/empty/test_m.html')
+  var html=hbs.renderView(path+'/empty/test_m.html', {name:"AAa"} )
+  res.send(html)
+
+
 
 })
 
@@ -131,4 +120,5 @@ app.get('/test',function(req,res){
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+ 
  
